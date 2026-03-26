41cecde599b0372e48980e3b9dd7e14c024e3776
Fiwpr06 <htfi597@gmail.com>
2026-03-26 19:33:32 +0700

    Fix host and port for Render deployment

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+import os
 from webapp.app import create_app
 
 
@@ -5,4 +6,5 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
